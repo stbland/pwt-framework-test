6b5e54c2ceaed9efef000c9f35df91f13a26d5f0
stephane <stephane@northern-light.stbland.lan>
2014-12-15 19:01:10 +0100

    Bash script to start embedded Tomcat server

--- scripts/mvn-tomcat7-run.sh
+++ scripts/mvn-tomcat7-run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
 SCRIPTPATH=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPTPATH")
-POMPATH=$SCRIPTDIR/../webapp/pom.xml
-mvn -f $POMPATH -Pgwt-codeserver gwt:compile tomcat7:run
+$SCRIPTDIR/doMaven-webapp.sh -Pgwt-draft-compile package tomcat7:run-war
